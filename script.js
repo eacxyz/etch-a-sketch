eae7792dea8011c6ec9b5b90f326fa961797b68b
Noah <100672306+eacxyz@users.noreply.github.com>
2023-10-22 03:34:22 -0400

    Add button to to generate new grid
    Allows the user to choose the number of squares per side in the grid,
    in the same total space as before. More squares mean smaller squares,
    and therefore more precise sketch experience. Max of 100 squares per
    side.

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 16 * 16; i++) {
-	const square = document.createElement('div');
-	square.className = 'square';
-	container.appendChild(square);
+function createGrid(perSide) {
+	const width = 640 / perSide;
+	for (let i = 0; i < perSide * perSide; i++) {
+		const square = document.createElement('div');
+		square.className = 'square';
+		square.style.minWidth = width + 'px';
+		square.style.height = width + 'px';
+		container.appendChild(square);
+	}
+
+	const squares = document.querySelectorAll('.square');
+	squares.forEach(square => square.addEventListener('mouseenter', function(e) {
+		e.target.style.backgroundColor = 'blue';
+	}));
 }
 
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => square.addEventListener('mouseenter', function(e) {
-	e.target.style.backgroundColor = 'blue';
-}))
+function clearGrid() {
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
+}
+
+function newGrid() {
+	let n = prompt('How many squares per side?');
+	while (n > 100) {
+		n = prompt('Maximum of 100, enter new number:')
+	}
+	while (n < 0) {
+		n = prompt('ERROR: negative squares not possible');
+	}
+	clearGrid();
+	createGrid(n);
+}
+
+createGrid(16);
